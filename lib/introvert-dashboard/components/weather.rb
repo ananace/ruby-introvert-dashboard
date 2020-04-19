fcf56423bd05637022ade9d0d5c0520760b273b4
Alexander Olofsson <ace@haxalot.com>
2020-04-19 21:11:11 +0200

    Clean up weather display, include precipitation

--- lib/introvert-dashboard/components/weather.rb
+++ lib/introvert-dashboard/components/weather.rb
@@ -78,13 +78,13 @@ module IntrovertDashboard::Components
     ]
 
     PRECIP_CATEGORIES = [
-      :none,
-      :snow,
-      :mix,
-      :rain,
-      :drizzle,
-      :freezing_rain,
-      :freezing_drizzle
+      nil,
+      'snow',
+      'snow and rain',
+      'rain',
+      'drizzle',
+      'freezing rain',
+      'freezing drizzle',
     ]
 
     def get_forecast(lat, lon)
@@ -114,16 +114,16 @@ module IntrovertDashboard::Components
             precipitation: {
               min: point[:parameters].find { |p| p[:name] == 'pmin' }[:values].first,
               max: point[:parameters].find { |p| p[:name] == 'pmax' }[:values].first,
-              category: point[:parameters].find { |p| p[:name] == 'pcat' }[:values].first,
+              category_id: point[:parameters].find { |p| p[:name] == 'pcat' }[:values].first,
             },
             humidity: point[:parameters].find { |p| p[:name] == 'r' }[:values].first,
-            symbol: point[:parameters].find { |p| p[:name] == 'Wsymb2' }[:values].first,
+            symbol_id: point[:parameters].find { |p| p[:name] == 'Wsymb2' }[:values].first,
           }
         end
 
         [today,tomorrow].each do |source|
           [:average, :average_day, :average_night].each do |target|
-            avg = source[target] = { pressure: 0, temperature: 0, wind: { dir: 0, speed: 0, gust_speed: 0 }, precipitation: { min: 0, max: 0, category: nil }, humidity: 0, symbol: nil }
+            avg = source[target] = { pressure: 0, temperature: 0, wind: { dir: 0, speed: 0, gust_speed: 0 }, precipitation: { min: 0, max: 0, category_id: nil }, humidity: 0, symbol_id: nil }
 
             overall = target == :average
             if overall
@@ -146,66 +146,34 @@ module IntrovertDashboard::Components
               %i[dir speed].each { |s| avg[:wind][s] = (avg[:wind][s] / count).round(2) }
               %i[min max].each { |s| avg[:precipitation][s] = (avg[:precipitation][s] / count).round(2) }
             end
-            avg[:precipitation][:category] = source[:by_hour].group_by{|i| i.dig :precipitation, :category }.max{|x,y| x[1].length <=> y[1].length}&.first
-            avg[:symbol] = source[:by_hour].group_by{|i| i.dig :symbol}.max{|x,y| x[1].length <=> y[1].length}&.first
+            avg[:precipitation][:category_id] = source[:by_hour].group_by{|i| i.dig :precipitation, :category_id }.max{|x,y| x[1].length <=> y[1].length}&.first
+            avg[:symbol_id] = source[:by_hour].group_by{|i| i.dig :symbol_id}.max{|x,y| x[1].length <=> y[1].length}&.first
           end
         end
 
         [today,tomorrow].each do |source|
           source[:by_hour].each do |h|
-            next unless h[:symbol].is_a? Numeric
-
-            symbols = h[:time].night? ? WEATHER_SYMBOLS_NIGHT : WEATHER_SYMBOLS_DAY
-            h[:symbol] = symbols[h[:symbol]]
+            if h[:symbol].nil?
+              symbols = h[:time].night? ? WEATHER_SYMBOLS_NIGHT : WEATHER_SYMBOLS_DAY
+              h[:symbol] = symbols[h[:symbol_id]]
+            end
+            if h.dig(:precipitation, :category).nil?
+              h[:precipitation][:category] = PRECIP_CATEGORIES[h[:precipitation][:category_id]]
+            end
           end
           %i[average average_day average_night].each do |avg|
-            symbols = avg == :average_night ? WEATHER_SYMBOLS_NIGHT : WEATHER_SYMBOLS_DAY
-            next if source.dig(avg, :symbol).nil?
-            next unless source.dig(avg, :symbol).is_a? Numeric
-
-            source[avg][:symbol] = symbols[source[avg][:symbol]]
+            if source.dig(avg, :symbol).nil?
+              symbols = avg == :average_night ? WEATHER_SYMBOLS_NIGHT : WEATHER_SYMBOLS_DAY
+              source[avg][:symbol] = symbols[source[avg][:symbol_id]]
+            end
+            if source.dig(avg, :precipitation, :category).nil?
+              source[avg][:precipitation][:category] = PRECIP_CATEGORIES[source[avg][:precipitation][:category_id]]
+            end
           end
         end
 
         { today: today, tomorrow: tomorrow }
       end.first
-    end
-
-    def render_card(doc)
-      doc.div.card.weather! do
-        doc.div class: 'card-header' do
-          doc.nav do
-            doc.ul.nav class: 'nav-xs nav-tabs card-header-tabs', role: :tablist do
-              doc.li class: 'nav-item mr-auto pr-4' do
-                doc.text 'Weather'
-              end
-              doc.li class: 'nav-item' do
-                doc.a.active.weatherTodayTab! class: 'nav-link', href: '#weatherToday', role: :tab, 'data-toggle': :tab, 'aria-controls': 'weatherToday', 'aria-selected': true, 'data-default': true do
-                  doc.text 'Today'
-                end
-              end
-              doc.li class: 'nav-item', 'data-duration': 30 do
-                doc.a.weatherTomorrowTab! class: 'nav-link', href: '#weatherTomorrow', role: :tab, 'data-toggle': :tab, 'aria-controls': 'weatherTomorrow', 'aria-selected': false do
-                  doc.text 'Tomorrow'
-                end
-              end
-            end
-          end
-        end
-
-        doc.div class: 'card-body tab-content' do
-          doc.div.show.active.weatherToday! class: 'tab-pane', role: :tabpanel, 'aria-labelledby': 'weatherTodayTab' do
-            doc.h5 class: 'card-title'
-            doc.h5 class: 'card-subtitle text-muted'
-            doc.p class: 'card-text'
-          end
-          doc.div.weatherTomorrow! class: 'tab-pane', role: :tabpanel, 'aria-labelledby': 'weatherTomorrowTab' do
-            doc.h5 class: 'card-title'
-            doc.h5 class: 'card-subtitle text-muted'
-            doc.p class: 'card-text'
-          end
-        end
-      end
     end
 
     get '/' do
